75fa04b48f39c577148fb121f5323d3c3bcbb142
Alfredjoy <alfredjoyhmn@gmail.com>
2020-12-11 15:21:02 +0530

    done with template and static views of the website..before setting up the data models

--- store/views.py
+++ store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def store(request):
-    return render(request,'store.html')
+    products = Product.objects.all()
+    context = {'products':products}
+    return render(request,'store.html',context)
 
 def cart(request):
     return render(request,'cart.html')
